11e17d694b7634b5bdf8c48fb05f2d8f3a8d21ce
PandaV <160627003+pandavio@users.noreply.github.com>
2025-04-27 16:45:50 +0800

    update port
    11184

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def analyze():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 11184))
     app.run(host="0.0.0.0", port=port)
